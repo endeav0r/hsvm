7eef8e158ae605b458d67971d30dbb6a402b2a65
endeav0r <endeavor@rainbowsandpwnies.com>
2012-09-03 14:48:45 -0500

    adding backdoor2 to make_programs.sh and clean_programs.sh

--- clean_programs.sh
+++ clean_programs.sh
@@ -1,6 +1,7 @@
 rm -f core
 rm -f hidden_key
 rm -f backdoor 
+rm -f backdoor2 
 rm -f key
 rm -f news
 rm -f players
--- make_programs.sh
+++ make_programs.sh
@@ -1,5 +1,6 @@
 ./assembler -o key programs/key.s stdlib/stdio.s
 ./assembler -o backdoor programs/backdoor.s stdlib/stdio.s
+./assembler -o backdoor2 programs/backdoor2.s stdlib/stdio.s
 ./assembler -o news programs/news.s stdlib/string.s stdlib/stdio.s
 ./assembler -o players programs/players.s programs/player.s stdlib/stdio.s stdlib/string.s stdlib/heap.s
 ./assembler -o hidden_key programs/hidden_key.s stdlib/string.s stdlib/stdio.s
